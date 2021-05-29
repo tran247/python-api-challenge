0a7632d2fdbb0aac8ee6209b97b8a6fc391c6d55
tran247 <75042393+tran247@users.noreply.github.com>
2021-05-28 23:37:39 -0400

    added csv file

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f2cd05e0389ac8134ade8adce4b69104"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBGvwCdJbxZ_ssDG1fupMMEirrmxrfUaDQ!"
